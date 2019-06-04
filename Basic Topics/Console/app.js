ea9f912177f39fe2c6a267501afe44002c16c197
zumrudu-anka <osmandurdag@hotmail.com>
2019-06-04 23:01:57 +0300

    Beginning to JS

---
+++ Basic Topics/Console/app.js
@@ -0,0 +1,20 @@
+console.log("Merhaba Javascript");
+console.log(213312);
+console.log(true);
+console.log(false);
+
+var a=20;
+
+console.log(a);
+
+console.log(["Ahmet","Mehmet"]);
+
+var b=["Ali","Veli"];
+var c="osman";
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+console.warn("Bu bir uyaridir!..");
+console.error("Bu bir hatadir!..");
+console.clear()
---
+++ Basic Topics/JS Beginning/app.js
@@ -0,0 +1,4 @@
+console.log("naber");
+alert("hosgeldin");
+var a=10;
+alert(a);
